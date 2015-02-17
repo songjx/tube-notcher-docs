4780e12081ed50c3614219697acf910c87848106
Jackie Song <lava.tomato@gmail.com>
2015-02-16 19:11:24 -0500

    print formatted latex output
    Prints proper formatted latex output to terminal. Assumes subassemblies
    go max 3 levels deep (to subsubsection).

--- spring-report/scripts/drawings.py
+++ spring-report/scripts/drawings.py
@@ -4,12 +4,17 @@ os.chdir('..')
 walk = os.walk('./drawings')
 
 #latex integration
-latexLevels = ['chapter','section','subsection','subsubsection']
+latexLevels = ['\chapter','\section','\subsection','\subsubsection']
 
 for roots, dirs, files in walk:
     level = os.path.relpath(roots).count(os.sep)
-    #print('root:', os.path.relpath(roots), '(level', level, ')')
-    print('heading:',latexLevels[level],os.path.basename(roots))
+    print(latexLevels[level],'{',os.path.basename(roots),'}',sep='')
     files = [os.path.splitext(file)[0] for file in files]
-    print('files:',files)
-    print('---')
+    #print('files:',files)
+    for file in files:
+        print('\n\includepdf[pages=-,')
+        print('pagecommand={\\thispagestyle{pdfpage}},')
+        print('scale=0.9,offset=0 -.25in,')
+        print('landscape=true]')
+        print('{',os.path.join(os.path.relpath(roots),file),'}',sep='')
+    print('\n%---\n')
