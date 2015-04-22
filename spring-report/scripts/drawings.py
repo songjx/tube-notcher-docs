bddb8c5bc305392640a44f397824112344f843b2
Jackie Song <lava.tomato@gmail.com>
2015-04-22 00:44:42 -0400

    fix drawing extension problem
    drawings.py gave drawing filenames to pdfpages without extensions; *.PDF files weren't being found. script now gives full names; *.pdf and *.PDF both work. need to address separator difference between windows and unix.

--- spring-report/scripts/drawings.py
+++ spring-report/scripts/drawings.py
@@ -11,11 +11,12 @@ with open('drawings.tex','w') as drawings:
         level = os.path.relpath(roots).count(os.sep)
         drawings.write(
             '{0}{{{1}}}\n\n'.format(latexLevels[level],os.path.basename(roots)))
-        files = [os.path.splitext(file)[0] for file in files]
+        files = [os.path.basename(file) for file in files]
         for file in files:
             drawings.write('\includepdf[pages=-,\n')
             drawings.write('pagecommand={\\thispagestyle{pdfpage}},\n')
             drawings.write('scale=0.9,offset=0 -.25in,\n')
             drawings.write('landscape=true]\n')
             drawings.write('{{{}}}\n\n'.format(
-                os.path.join(os.path.relpath(roots),file).replace(os.sep,os.altsep)))
+                #os.path.join(os.path.relpath(roots),file).replace(os.sep,os.altsep)))
+                os.path.join(os.path.relpath(roots),file)))
